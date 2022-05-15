58ad55e2600bd3db2fd661fc672e9da19f4de068
saket105 <saketcool35@gmail.com>
2022-05-16 00:05:31 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require("./app");
 const dotEnv = require("dotenv");
-
+const connectDatabase = require("./config/database");
 // config
 dotEnv.config({ path: "backend/config/config.env" });
+
+// connecting to database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
